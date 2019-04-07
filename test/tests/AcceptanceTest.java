47f9ab233c6ca667ac8a66313dd907e7829a532f
Hadi <hadihorani@hotmail.com>
2019-04-07 16:53:31 +0200

    Included jar files to project

--- test/tests/AcceptanceTest.java
+++ test/tests/AcceptanceTest.java
@@ -1,9 +1,9 @@
 package tests;
 
 import org.junit.runner.RunWith;
-import cucumber.api.CucumberOptions;
-import cucumber.api.SnippetType;
-import cucumber.api.junit.Cucumber;
+
+import cucumber.api.*;
+import cucumber.api.junit.*;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "use_cases", 
