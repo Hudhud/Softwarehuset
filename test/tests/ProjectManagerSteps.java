15ce66c476391d2f7694ee0192f13a047866b223
s184235 <s184235@student.dtu.dk>
2019-04-08 16:13:00 +0200

    create activity feature file + test

---
+++ test/tests/ProjectManagerSteps.java
@@ -0,0 +1,43 @@
+package tests;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ProjectManagerSteps {
+	@Then("the system provides an error message ?The project has no name. Please choose a name for the project?")
+	public void theSystemProvidesAnErrorMessageTheProjectHasNoNamePleaseChooseANameForTheProject() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Given("that the project manager provides the ID {int} for a project")
+	public void thatTheProjectManagerProvidesTheIDForAProject(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("the project manager creates an activity")
+	public void theProjectManagerCreatesAnActivity() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("the system creates an activity with a consecutive number")
+	public void theSystemCreatesAnActivityWithAConsecutiveNumber() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Given("that the project manager provides the ID of a project, which has not been created")
+	public void thatTheProjectManagerProvidesTheIDOfAProjectWhichHasNotBeenCreated() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("the system provides an error message ?A project with the given ID does not exist?")
+	public void theSystemProvidesAnErrorMessageAProjectWithTheGivenIDDoesNotExist() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+}
