d9cd7ff14c8c9de4439fc74b78ec342f27e79824
s184235 <s184235@student.dtu.dk>
2019-04-14 13:59:05 +0200

    projectManager klassen

---
+++ src/employee/ProjectManager.java
@@ -0,0 +1,20 @@
+package employee;
+
+public class ProjectManager extends Employee{
+
+
+	
+	public ProjectManager(String employeeID) {
+		super(employeeID);
+	}
+
+
+	public void createAct() {
+		
+	}
+	
+	
+	public void AssignEmp() {
+		
+	}
+}
