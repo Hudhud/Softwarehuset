d070b8c39ba08340a91bc6b66a5d96df862433a5
Hadi <hadihorani@hotmail.com>
2019-04-22 15:45:23 +0200

    Feature to_be_PM is fully implemented and tested

--- target/cucumber/wikipedia.html/report.js
+++ target/cucumber/wikipedia.html/report.js
@@ -20,11 +20,11 @@ formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "provides the project id \"2019000001\"",
+  "name": "provides the project id \"2019000001\" along with the project name \"TestProject1\"",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CEOSteps.providesTheProjectId(String)"
+  "location": "CEOSteps.providesTheProjectIdAlongWithTheProjectName(String,String)"
 });
 formatter.result({
   "status": "passed"
@@ -55,6 +55,116 @@ formatter.step({
 });
 formatter.match({
   "location": "CEOSteps.theSystemMakesTheEmployeeAProjectManager()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Choose an employee to be a project manager when the employee does not exist",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "the CEO provides his id \"ceo\"",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "CEOSteps.theCEOProvidesHisId(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "provides the project id \"2019000001\" along with the project name \"TestProject1\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "CEOSteps.providesTheProjectIdAlongWithTheProjectName(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the employee with the ID \"abcd\" does not exist",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "CEOSteps.theEmployeeWithTheIDDoesNotExist(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the CEO chooses an employee with an ID \"abcd\" to be a project manager for the project with the ID \"2019000001\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "CEOSteps.theCEOChoosesAnEmployeeWithAnIDToBeAProjectManagerForTheProjectWithTheID(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the system provides an error message \"This employee does not exist\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "EmployeeSteps.theSystemProvidesAnErrorMessage(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Choose an employee to be a project manager when a wrong CEO id is provided",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "that a wrong CEO id \"aaa\" is provided",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "CEOSteps.thatAWrongCEOIdIsProvided(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "provides the project id \"2019000001\" along with the project name \"TestProject1\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "CEOSteps.providesTheProjectIdAlongWithTheProjectName(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the employee with the ID \"abcd\" exists",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "CEOSteps.theEmployeeWithTheIDExists(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the CEO chooses an employee with an ID \"abcd\" to be a project manager for the project with the ID \"2019000001\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "CEOSteps.theCEOChoosesAnEmployeeWithAnIDToBeAProjectManagerForTheProjectWithTheID(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the system provides an error message \"This operation can only be performed by the CEO\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "EmployeeSteps.theSystemProvidesAnErrorMessage(String)"
 });
 formatter.result({
   "status": "passed"
