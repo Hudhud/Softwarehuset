3827a980e63163fd6bc9fb3d3f51a54542995053
Hadi <hadihorani@hotmail.com>
2019-04-22 15:50:44 +0200

    Code cleanup

--- src/employee/Employee.java
+++ src/employee/Employee.java
@@ -1,14 +1,10 @@
 package employee;
-import java.util.ArrayList;
-import java.util.Date;
-
 import project.Project;
 
 public class Employee {
 	
 	private String employeeID;
 	private boolean isEmployeePM;
-	private boolean isSignedIn;
 	private boolean vacant;
 	
 	// This empty constructor is necessary, because sometimes we need it in some methods. 
@@ -36,14 +32,6 @@ public class Employee {
 		this.isEmployeePM = isEmployeePM;
 	}
 	
-	public boolean getIsSignedIn() {
-		return isSignedIn;
-	}
-	
-	public void setIsSignedIn(boolean isSignedIn) {
-		this.isSignedIn = isSignedIn; 
-	}
-	
 	public void setIsEmployeeVacant(boolean vacant) {
 		this.vacant = vacant;
 	}
@@ -51,11 +39,7 @@ public class Employee {
 	public boolean isEmployeeVacant() {
 		return vacant;
 	}
-	
-//	public boolean checkEmployeeId(String employeeID) {
-//		return allEmployeeIDs.contains(employeeID);
-//	}
-	
+		
 	public Project createProj(String name, String id) throws Exception {
 		Project p = new Project(name, id);
 		return p;
--- target/cucumber/wikipedia.html/report.js
+++ target/cucumber/wikipedia.html/report.js
@@ -1,4 +1,131 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("use_cases\\to_be_PM.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("use_cases\\create_a_new_activity.feature");
+formatter.feature({
+  "name": "Create a new activity",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "create an activity for a project",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "that the project manager is signed in",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "provides the ID 123 for a project",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "provides a name \"firstAct\" for the activity",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "the project manager creates an activity",
+  "keyword": "When "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "the system creates an activity with a consecutive number",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.uri("use_cases\\create_a_new_project.feature");
+formatter.feature({
+  "name": "create a new project",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "Create a new project",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "that a name \"Project_1\" for the project is provided",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "EmployeeSteps.thatANameForTheProjectIsProvided(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "a new project is created",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "EmployeeSteps.a_new_project_is_created()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the system creates a project with the provided name \"Project_1\" and a consecutive number",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "EmployeeSteps.theSystemCreatesAProjectWithTheProvidedNameAndAConsecutiveNumber(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Create a new project without name",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "that a name for the project is not provided",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "EmployeeSteps.thatANameForTheProjectIsNotProvided()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "a new project is created",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "EmployeeSteps.a_new_project_is_created()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the system provides an error message \"The project has no name. Please choose a name for the project\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "EmployeeSteps.theSystemProvidesAnErrorMessage(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.uri("use_cases\\to_be_PM.feature");
 formatter.feature({
   "name": "Choose an employee to be a project manager",
   "description": "",
